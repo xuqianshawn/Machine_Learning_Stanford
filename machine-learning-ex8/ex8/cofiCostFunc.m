878335f36b71c982d4c07924f2632ec9c773d4a8
xuqianshawn <xuqianshawn@gmail.com>
2017-09-28 16:16:14 +0800

    completion of week 9

--- machine-learning-ex8/ex8/cofiCostFunc.m
+++ machine-learning-ex8/ex8/cofiCostFunc.m
@@ -41,17 +41,20 @@ Theta_grad = zeros(size(Theta));
 %
 
 
+J = (1/2).*sum(sum(((X*Theta').*R-Y.*R).^2))+(lambda./2.*sum(sum(Theta.^2)))+(lambda./2.*sum(sum(X.^2)));	
+% Only predict rating X*Theta' if user has rated (i.e. R=1)
 
+X_grad = (((X*Theta').*R*Theta-Y.*R*Theta)+lambda.*X);
+Theta_grad = ((X'*((X*Theta').*R)-X'*(Y.*R)))'+lambda.*Theta;
 
-
-
-
-
-
-
-
-
-
+% Alternative according to exercise handout
+%[r,c]=size(R);
+%for i=1:r
+%	idx = find(R(i,:)==1);
+%	Theta_temp = Theta(idx,:);
+%	Y_temp = Y(i,idx);
+%	X_grad(i,:) = (X(i,:)*Theta_temp'-Y_temp)*Theta_temp;
+%end
 
 
 
